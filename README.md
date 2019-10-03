f2a1fe7748e1fcc4fe080bc059f99ea7ceb93ad0
Vahag <vahag_saribekyab@mail.ru>
2019-10-03 21:18:30 +0400

    update read.me

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# simple-instagram-task
-Service returns some information(followers, likes, etc...) about specified user
+# This repository contains simple NodeJS ervice which returns some information(followers, likes, etc...) about specified user of Instagram
+
+## Run commands
+
+1. To start the app locally you need to install have NodeJS installed on your machine
+
+	. First run 'npm install'
+	. Second run 'npm start'
+
+2. Second approach is to run the application with docker container
+
+	# Build docker image with application
+		docker build -t instagram-getter:0.0.1
+	# Run docker image
+		docker run instagram-getter:0.0.1
+
--- controllers/users.js
+++ controllers/users.js
@@ -3,8 +3,11 @@ const instagramClient = new Instagram({username: '', password: ''});
 
 module.exports = async (req, res) => {
 	try {
+		console.log('Request received on users endpoint with parameters: ', JSON.stringify(req.params))
+
     	const user = await instagramClient.getUserByUsername({ username: req.params.username });
     	const posts = [];
+    	// We have to return image url and id for the first 6 posts of user if they are exist
     	for (let i = 0; i < user.edge_owner_to_timeline_media.edges.length && i < 6; i++) {
     		posts.push({
     			url: user.edge_owner_to_timeline_media.edges[i].node.display_url,
@@ -20,8 +23,7 @@ module.exports = async (req, res) => {
     		posts,
     	});
 	} catch(err) {
-		console.error('Error occured while fetching data from instagram. request parameters: ', JSON.stringify(req.params));
-		console.error('error: ', err.message);
+		console.error('Error occured while fetching data from instagram: ', err.message);
 		res.status(500).end('Internal server error');
 	}
 };
